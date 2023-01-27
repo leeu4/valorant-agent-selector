86d1867b94ae89669f8d39150e9eebcc5ff29321
leeu4 <123780953+leeu4@users.noreply.github.com>
2023-01-28 01:57:41 +0300

    Add files via upload

---
+++ agent-selector.py
@@ -0,0 +1,249 @@
+import time as t
+import pyautogui as auto
+
+agent = input("Enter the valorant agent:(to stop the loop move the mouse to top left!!) ")
+
+
+if agent == "reyna":
+    while True:
+     auto.leftClick(819,1009)
+     t.sleep(0.2)
+     auto.doubleClick(940,810)
+
+
+
+elif agent == "astra":
+    while True:
+        auto.leftClick(571,911)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+
+elif agent == "breach":
+    while True:
+        auto.leftClick(823,911)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+
+elif agent == "brimstone":
+    while True:
+     auto.leftClick(912,917)
+     t.sleep(0.2)
+     auto.doubleClick(940, 810)
+
+
+elif agent == "chamber":
+    while True:
+     auto.leftClick(991,919)
+     t.sleep(0.2)
+     auto.doubleClick(940, 810)
+
+
+elif agent == "cypher":
+    while True:
+        auto.leftClick(1082,925)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+elif agent == "fade":
+    while True:
+        auto.leftClick(1161,920)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+elif agent == "jett":
+    while True:
+        auto.leftClick(1257,916)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+elif agent == "kayo":
+    while True:
+        auto.leftClick(1322,913)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+elif agent == "killjoy":
+    while True:
+        auto.leftClick(1243,927)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+elif agent == "neon":
+    while True:
+     auto.leftClick(1337,915)
+     t.sleep(0.2)
+     auto.doubleClick(940, 810)
+elif agent == "omen":
+    while True:
+        auto.leftClick(567,1004)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+elif agent == "phoniex":
+    while True:
+        auto.leftClick(664,997)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+elif agent == "raze":
+    while True:
+        auto.leftClick(743,997)
+        t.sleep(0.2)
+        auto.doubleClick(940, 810)
+elif agent == "sage":
+    while True:
+        auto.leftClick(910,991)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+elif agent == "skye":
+    while True:
+     auto.leftClick(999,995)
+     t.sleep(0.2)
+     auto.doubleClick(940, 810)
+
+elif agent == "sova":
+    while True:
+        auto.leftClick(1168,995)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+elif agent == "viper":
+    while True:
+        auto.leftClick(1234,995)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+elif agent == "yoru":
+    while True:
+        auto.leftClick(1328,995)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+elif agent == "harbor":
+    while True :
+        auto.leftClick(1428,995)
+        t.sleep(0.2)
+        auto.doubleClick(940,810)
+
+
+else:
+    while True:
+     agent = input("make sure to spell the agent's name correctly (no upper-case letters) ")
+
+     if agent == "reyna":
+         while True:
+             auto.leftClick(819, 1009)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+
+
+     elif agent == "astra":
+         while True:
+             auto.leftClick(571, 911)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+
+     elif agent == "breach":
+         while True:
+             auto.leftClick(823, 911)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+
+     elif agent == "brimstone":
+         while True:
+             auto.leftClick(912, 917)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+
+     elif agent == "chamber":
+         while True:
+             auto.leftClick(991, 919)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+
+     elif agent == "cypher":
+         while True:
+             auto.leftClick(1082, 925)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+     elif agent == "fade":
+         while True:
+             auto.leftClick(1161, 920)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+     elif agent == "jett":
+         while True:
+             auto.leftClick(1257, 916)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+     elif agent == "kayo":
+         while True:
+             auto.leftClick(1322, 913)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+     elif agent == "killjoy":
+         while True:
+             auto.leftClick(1243, 927)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+     elif agent == "neon":
+         while True:
+             auto.leftClick(1337, 915)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+     elif agent == "omen":
+         while True:
+             auto.leftClick(567, 1004)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+     elif agent == "phoniex":
+         while True:
+             auto.leftClick(664, 997)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+     elif agent == "raze":
+         while True:
+             auto.leftClick(743, 997)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+     elif agent == "sage":
+         while True:
+             auto.leftClick(910, 991)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+     elif agent == "skye":
+         while True:
+             auto.leftClick(999, 995)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+     elif agent == "sova":
+         while True:
+             auto.leftClick(1168, 995)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+     elif agent == "viper":
+         while True:
+             auto.leftClick(1234, 995)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+     elif agent == "yoru":
+         while True:
+             auto.leftClick(1328, 995)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
+     elif agent == "harbor":
+         while True:
+             auto.leftClick(1428, 995)
+             t.sleep(0.2)
+             auto.doubleClick(940, 810)
+
